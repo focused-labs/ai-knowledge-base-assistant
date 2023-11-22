2de472273b843fcb5150013be3e6de8c3e8c3ea3
kaylynnwatson <kaylynnwatson@focusedlabs.io>
2023-11-22 04:29:23 -0500

    Experimented with using llama hub reader for notion

--- import_service.py
+++ import_service.py
@@ -1,14 +1,22 @@
+import http.client
+import json
 import os
 
 import requests
 from PyPDF2 import PdfReader, PdfWriter
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from llama_index.readers import NotionPageReader
 
 from config import PROCESSED_DATA_DIRECTORY
 
+load_dotenv()
+
+NOTION_INTEGRATION_TOKEN = os.getenv("NOTION_INTEGRATION_TOKEN")
 PDF_DIRECTORY = './raw_notion_pdfs'
 WEBSITE_DIRECTORY = './raw_website_scrape'
 COMBINED_PDF = os.path.join(PROCESSED_DATA_DIRECTORY, "notion_docs_combined.pdf")
+COMBINED_NOTION_TEXT_FILE = os.path.join(PROCESSED_DATA_DIRECTORY, 'combined_notion_import.txt')
 MAX_FILE_SIZE = 512
 MAX_NUM_FILES = 24
 
@@ -33,6 +41,37 @@ def parse_and_combine_pdfs():
 
     if get_file_size(COMBINED_PDF) > MAX_FILE_SIZE:
         print("ERROR - Combination of all notion pdfs is too large of a file. Please parse into smaller pdfs")
+
+
+def get_notion_page_title(page_id):
+    try:
+        headers = {'Authorization': f'Bearer {NOTION_INTEGRATION_TOKEN}', 'Notion-Version': '2022-06-28'}
+        connection = http.client.HTTPSConnection("api.notion.com")
+
+        connection.request("GET", f"/v1/pages/{page_id}/properties/title", headers=headers)
+        page_title = json.loads(connection.getresponse().read())
+
+        return page_title['results'][0]['title']['plain_text']
+    except Exception as e:
+        print(f"Failed to retrieve notion metadata{e} for page id: {page_id}")
+        return ""
+
+
+def read_notion(page_ids: list):
+    documents = NotionPageReader(integration_token=NOTION_INTEGRATION_TOKEN).load_data(
+        page_ids=page_ids
+    )
+    documents = [
+        doc.to_langchain_format()
+        for doc in documents
+    ]
+    with open(COMBINED_NOTION_TEXT_FILE, 'a') as file:
+        for doc in documents:
+            metadata = get_notion_page_title(doc.metadata['page_id'])
+            content = doc.page_content
+            file.write(f"# {metadata}\n{content}\n\n")
+
+    print("Documents processed and appended to combined_notion_import.txt")
 
 
 def save_website_scrape(urls: list):
